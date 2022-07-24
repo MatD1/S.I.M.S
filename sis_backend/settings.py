8f70b497be924960d8de18ddcfec1565705ab6da
Mathew Doak <64875762+MatD1@users.noreply.github.com>
2022-07-24 17:04:16 +1000

    Unblock api restrictions

--- sis_backend/settings.py
+++ sis_backend/settings.py
@@ -146,9 +146,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-    )
+    #'DEFAULT_AUTHENTICATION_CLASSES': (
+   #     'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+   # )
 }
 
 # DJANGO REST API KEY CUSTOM HEADER
